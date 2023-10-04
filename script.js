1bfd8c5fc8c084ccda642d7514184c560dae1eb0
Adriano Gomes <69825351+AdrianoGomesFilho@users.noreply.github.com>
2023-10-04 20:21:07 -0300

    very interesting

---
+++ script.js
@@ -0,0 +1,22 @@
+const html = document.querySelector('html')
+const focoButton = document.querySelector('.app__card-button--foco')
+const curtoButton = document.querySelector('.app__card-button--curto')
+const longoButton = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+
+focoButton.addEventListener('click', () => {
+    alterarContexto('foco')
+})
+
+curtoButton.addEventListener('click', () => {
+    alterarContexto('descanso-curto')
+})
+
+longoButton.addEventListener('click', () => {
+    alterarContexto('descanso-longo')
+})
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
