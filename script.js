b9c57be26e5104b93acac4db24bae990d97d9651
Adriano Gomes <69825351+AdrianoGomesFilho@users.noreply.github.com>
2023-10-10 20:38:12 -0300

    .

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const longoButton = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
+
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('sons/luna-rise-part-one.mp3')
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null
+
+musica.loop = true;
+const botoesCard = document.querySelectorAll('.appCardButton')
+
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoButton.addEventListener('click', () => {
     alterarContexto('foco')
@@ -27,7 +46,7 @@ function alterarContexto(contexto) {
     botoes.forEach(function (contexto) {
         contexto.classList.remove('active')
     })
-    
+
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -48,4 +67,29 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        zerar()
+        alert('Tempo finalizado')
+        return
+    }
+    tempoDecorridoEmSegundos -= 1;
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if(intervaloId) {
+        zerar()
+        return
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000) //callback vai ser chamado a cada 1 seg
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
