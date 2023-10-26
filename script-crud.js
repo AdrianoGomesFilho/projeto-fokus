0206f7665868b72ef59ad8d8c08b61b5e1db8cd3
Adriano Gomes <69825351+AdrianoGomesFilho@users.noreply.github.com>
2023-10-26 20:05:49 -0300

    .

--- script-crud.js
+++ script-crud.js
@@ -58,10 +58,24 @@ function createTask(tarefa) {
 
     paragraph.textContent = tarefa.descricao
 
+    const button = document.createElement('button')
+
     li.onclick = () => {
         selecionaTarefa(tarefa, li)
 
     }
+
+    svgIcon.addEventListener('click', (event) => {
+        event.stopPropagation()
+        button.setAttribute('disabled', true)
+        li.classList.add('app__section-task-list-item-complete')
+    })
+
+    if(tarefa.concluida) {
+        button.setAttribute('disable', true)
+        li.classList.add('app__section-task-list-item-complete')
+    }
+    
 
     li.appendChild(svgIcon)
     li.appendChild(paragraph)
